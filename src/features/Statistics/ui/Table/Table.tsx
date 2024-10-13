19c2ffa23bc44022ebf40c9e44243b8582c560fa
Cooper Lee Simon <callmecooperlee@gmail.com>
2024-10-13 23:03:55 +0600

    Add StatisticsWidget.tsx

--- src/features/Statistics/ui/Table/Table.tsx
+++ src/features/Statistics/ui/Table/Table.tsx
@@ -1,21 +1,6 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { statisticsActions } from "@features/Statistics/slice/statisticsSlice.ts";
-import {
-    selectStatisticsData,
-    selectStatisticsError,
-    selectStatisticsIsLoading,
-} from "@features/Statistics/selectors/statisticsSelectors.ts";
-
 import cls from "./Table.module.scss";
 
-export const Table = () => {
-    const dispatch = useDispatch();
-    const statistics = useSelector(selectStatisticsData);
-
-    useEffect(() => {
-        dispatch(statisticsActions.statisticsRequest({ search: "" }));
-    }, []);
+export const Table = ({ statistics }) => {
     return (
         <table className={cls.table}>
             <thead>
---
+++ src/widgets/StatisticsWidget/StatisticsWidget.tsx
@@ -0,0 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+    selectStatisticsData,
+    selectStatisticsError,
+    selectStatisticsIsLoading,
+} from "@features/Statistics/selectors/statisticsSelectors.ts";
+import { useEffect } from "react";
+import { statisticsActions } from "@features/Statistics/slice/statisticsSlice.ts";
+import { Table } from "@features/Statistics/ui/Table/Table.tsx";
+import { Loader } from "@shared/ui/Loader/Loader.tsx";
+
+export const StatisticsWidget = () => {
+    const dispatch = useDispatch();
+    const statistics = useSelector(selectStatisticsData);
+    const isStatisticsLoading = useSelector(selectStatisticsIsLoading);
+
+    useEffect(() => {
+        dispatch(statisticsActions.statisticsRequest({ search: "" }));
+    }, []);
+
+    return !isStatisticsLoading ? (
+        <Table statistics={statistics} />
+    ) : (
+        <Loader />
+    );
+};
