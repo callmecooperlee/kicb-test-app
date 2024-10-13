02e0b060ad28b1d6f71b36db984a83883e84346d
Cooper Lee Simon <callmecooperlee@gmail.com>
2024-10-13 23:47:21 +0600

    Add Suspense

--- src/widgets/CRMLayout/CRMLayout.tsx
+++ src/widgets/CRMLayout/CRMLayout.tsx
@@ -6,20 +6,26 @@ import { Sidebar } from "@features/Sidebar/Sidebar.tsx";
 import { Search } from "@shared/ui/Search/Search.tsx";
 
 import cls from "./CRMLayout.module.scss";
+import { Suspense } from "react";
+import { Loader } from "@shared/ui/Loader/Loader.tsx";
 
 export const CRMLayout = () => {
     return (
         <div className={cls.container}>
-            <Sidebar />
-            <div className={cls.wrap}>
-                <div className={cls.header}>
-                    <Search />
-                    <ProfileButton />
+            <Suspense>
+                <Sidebar />
+            </Suspense>
+            <Suspense fallback={<Loader />}>
+                <div className={cls.wrap}>
+                    <div className={cls.header}>
+                        <Search />
+                        <ProfileButton />
+                    </div>
+                    <div className={cls.content}>
+                        <Outlet />
+                    </div>
                 </div>
-                <div className={cls.content}>
-                    <Outlet />
-                </div>
-            </div>
+            </Suspense>
         </div>
     );
 };
